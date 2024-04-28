158a36c0c969a1bd026c34745ce53c1768951b1b
RandomAiPerson <156617164+RandomAiPerson@users.noreply.github.com>
2024-04-27 21:29:53 -0400

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,14 +5,7 @@ function Footer() {
     return (
         <footer class="footer px-7 mt-auto mb-5 select-none flex items-center justify-between">
             <div>
-                <Obfuscated>© Metallic 2022</Obfuscated> - {new Date().getFullYear()}
-            </div>
-            <div class="flex items-center justify-center gap-1.5">
-                <a class="hover:underline" href={github}><Obfuscated>GitHub</Obfuscated></a>
-                <span>/</span>
-                <a class="hover:underline" href={discord}><Obfuscated>Discord</Obfuscated></a>
-                <span>/</span>
-                <a class="hover:underline" href="/privacy">Privacy</a>
+                <Obfuscated>© Acustic 2024</Obfuscated> - {new Date().getFullYear()}
             </div>
         </footer>
     )
